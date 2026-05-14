249c87a255b9a9a1495e9070a811da0de2bdd18f
Mihna T <mihna@cybrosys.com>
2026-05-14 10:44:15 +0530

    [UPDT] version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Delivery Map',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Inventory',
     'summary': 'Delivery map in stock picking',
     'author': 'Mihna',

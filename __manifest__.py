6858b763be9a0a7574c4bfa9e2b86e8e2d104513
Mihna T <mihna@cybrosys.com>
2026-05-14 12:12:44 +0530

    [UPDATE]manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'POS Refund Limit Days',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Sales/Point of Sale',
     'summary': 'Restrict POS order refunds by enforcing a configurable return period in days.',
     'description': """
